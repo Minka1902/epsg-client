e22e63da5f5bead90f37e04f369b3c54a65dba2a
Michael Scharff <minka.scharff@gmail.com>
2023-04-04 15:45:58 +0300

    Fixing misspel

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -9,40 +9,6 @@ import DropdownControl from '../dropdownControl/DropdownControl';
 import EpsgForm from '../form/EpsgForm'
 import Pointer from '../../images/pointer.svg';
 
-// function Table(props) {
-//   const { tableHeaders, data, coordinates } = props;
-
-//   return (
-//     <div className="table-container">
-//       <table>
-//         <thead>
-//           <tr>
-//             {tableHeaders.map((header, index) => {
-//               return <th key={index}>{header}</th>
-//             })}
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {data.map((item, index) => (
-//             <tr key={index}>
-//               <td>{index + 1}</td>
-//               <td>{item.epsg}</td>
-//               <td>{item.distance ? item.distance : 'Unknown'}</td>
-//             </tr>
-//           ))}
-//         </tbody>
-//         {coordinates ? <tfoot>
-//           <tr>
-//             <td>licked</td>
-//             <td>Latitude: {coordinates.latitude}</td>
-//             <td>Longtitude: {coordinates.longtitude}</td>
-//           </tr>
-//         </tfoot> : <></>}
-//       </table>
-//     </div>
-//   );
-// };
-
 export default function App() {
   const [coords, setCoords] = React.useState([31.89291, 35.03254]);
   const [copyCoords, setCopyCoords] = React.useState(null);
